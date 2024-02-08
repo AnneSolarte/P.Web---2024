6fb3e10cb1d67c74e4c98862727fd153535f690d
Anne Solarte <anne.solarte99@gmail.com>
2024-02-08 16:41:57 -0500

    BlockTextG502

---
+++ LogitechPage/src/components/BlockTextG512/BlockTextG512.jsx
@@ -0,0 +1,14 @@
+
+import { Text } from '../Text/Text';
+
+export default function BlockTextG512() {
+  return (
+    <section className='BlockTextG512'>
+          <Text class='title' text='G502 HERO WIRELESS'/>
+          <Text class='subTitle' text="Logitech's High Performance Wireless Gaming Mouse"/>
+          <Text class='text' text='G502 HERO features an advanced optical sensor for maximum tracking accuracy, customizable RGB lighting, custom game profiles, from 200 up to 25,600 DPI, and repositionable weights.'/>
+          <Text class='price' text='INR 4999'/>
+
+    </section> 
+  );    
+}
