0791bf0118c9b9b9b4af8eb257733ff64cc048f9
Anne Solarte <anne.solarte99@gmail.com>
2024-04-28 18:15:59 -0500

    fixed: option multiple categories in data projects

--- Portfolio/src/data/projects.js
+++ Portfolio/src/data/projects.js
@@ -9,52 +9,68 @@ const projects = [
     title: 'MIXME',
     name: 'mixme',
     description: 'MIXME is a mobile app that empowers users to search, filter, index and buy outfits and clothes from big brands, fashion influencers or any other user of the app. ',
-    category: 'UX Design',
     project: 'Branding and app design',
     behanceLink: 'Link to Behance',
     image1: mixmeImg1,
     image2: 'url/to/image2.jpg',
     image3: 'url/to/image3.jpg',
-    image4: 'url/to/image4.jpg'
+    image4: 'url/to/image4.jpg',
+    categories: {
+      'UX Design': true,
+      'UI Design': false,
+      'FrontEnd Dev': false
+    }
   },
   {
     id: 2,
     title: 'PRODUCTIVITY APP',
     name: 'productivityapp',
     description: 'Description for Project 2',
-    category: 'UI Design',
     project: 'Link to the project',
     behanceLink: 'Link to Behance',
     image1: proAppImg1,
     image2: 'url/to/image2.jpg',
     image3: 'url/to/image3.jpg',
-    image4: 'url/to/image4.jpg'
+    image4: 'url/to/image4.jpg',
+    categories: {
+      'UX Design': false,
+      'UI Design': true,
+      'FrontEnd Dev': false
+    }
   },
   {
     id: 3,
     title: 'GEOME3D',
     name: 'geome3d',
     description: 'Description for Project 2',
-    category: 'UI Design',
     project: 'Link to the project',
     behanceLink: 'Link to Behance',
     image1: geome3dImg1,
     image2: 'url/to/image2.jpg',
     image3: 'url/to/image3.jpg',
-    image4: 'url/to/image4.jpg'
+    image4: 'url/to/image4.jpg',
+    categories: {
+      'UX Design': false,
+      'UI Design': true,
+      'FrontEnd Dev': false
+    }
   },
   {
     id: 4,
     title: 'BOP IT',
     name: 'bopit',
     description: 'Description for Project 2',
-    category: 'UI Design',
     project: 'Link to the project',
     behanceLink: 'Link to Behance',
     image1: bopitImg1,
     image2: 'url/to/image2.jpg',
     image3: 'url/to/image3.jpg',
-    image4: 'url/to/image4.jpg'
+    image4: 'url/to/image4.jpg',
+    categories: {
+      'UX Design': false,
+      'UI Design': true,
+      'FrontEnd Dev': false
+    }
   }
 ]
 
