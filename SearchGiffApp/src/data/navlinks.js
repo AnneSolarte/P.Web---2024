6fa786d0497a69caa34172d550ffe7beebad4602
Anne Solarte <anne.solarte99@gmail.com>
2024-05-16 16:42:46 -0500

    fixed: iconsActives deleted

--- SearchGiffApp/src/data/navlinks.js
+++ SearchGiffApp/src/data/navlinks.js
@@ -1,21 +1,17 @@
 import iconHome from '../assets/iconHome.png'
 import iconFav from '../assets/iconFav.png'
-import violetIconHome from '../assets/activeIconHome.png'
-import violetIconFav from '../assets/activeIconFav.png'
 
 export const navClient = [
   {
     id: '1',
     text: 'Home',
     url: '/home',
-    icon: iconHome,
-    activeIcon: violetIconHome
+    icon: iconHome
   },
   {
     id: '2',
     text: 'Favorites',
     url: '/favorites',
-    icon: iconFav,
-    activeIcon: violetIconFav
+    icon: iconFav
   }
 ]
